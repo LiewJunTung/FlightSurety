6e2b0ecfe6f08adece0b8234e00f390530e6725b
Liew Jun Tung <juntung@netvirta.com>
2021-10-30 18:15:18 +0800

    build: init setup

--- truffle.js
+++ truffle.js
@@ -1,14 +1,13 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "defense biology quote light blood lift human identify chest kit invite nice";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
     }
   },
   compilers: {
